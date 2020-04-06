26f843e68fdbb2bb1627abd83584b7c41e85bc96
Stefan Balauca <57128020+stefanrzv2000@users.noreply.github.com>
2020-04-07 02:24:55 +0300

    webpy tests and image processing

--- week2/split.py
+++ week2/split.py
@@ -2,6 +2,9 @@ import os
 import numpy as np
 from PIL import Image
 
+"""
+returns patches, a matrix of PIL.Image objects of the given size
+"""
 def split_image(img, size = (50,50)):
     maxX, maxY = img.width, img.height
 
@@ -12,13 +15,13 @@ def split_image(img, size = (50,50)):
 
     new_im = Image.new('RGB',(cols*size[0],rows*size[1]),color=(255,255,255))
 
-    new_im.paste()
+    new_im.paste(img)
 
     print(len(patches),len(patches[0]))
 
     for x in range(1,maxX,50):
         for y in range(1,maxY,50):
-            patch = img.crop((x,y,x+50,y+50))
+            patch = new_im.crop((x,y,x+50,y+50))
             patches[x//50][y//50] = patch
 
     return patches
