44973b2c115fc2a756b55b868e3de61395b80856
rs98xi <47506682+rs98xi@users.noreply.github.com>
2020-05-11 23:25:40 +0300

    Add files via upload

--- Framework/IP-Framework/EpidemyAlert.cs
+++ Framework/IP-Framework/EpidemyAlert.cs
@@ -1,28 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Numerics;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
+using IP_Framework.InternalDbHandler;
+using MongoDB.Bson;
 
 namespace IP_Framework
 {
     class EpidemyAlert : IModule
     {
         private EventHandler fatherHandler;
-        private String text = "EpidemyAlert constructor";
 
         public static double AreaAroundYuu = 0.2;
-        public static double NeighourHood = 1.5;
-        public static double Town = 10;
-        public static double Country = 100;
+        public static int AreaAroundYuuCases = 8;
 
+        public static double NeighourHood = 1.5;
+        public static int NeighourHoodCases = 20;
+
+        public static double Town = 10;
+        public static int TownCases = 150;
+
+        public static double Country = 100;
+        public static int CountryCases = 1000;
 
         public EpidemyAlert(EventHandler father)
         {
             fatherHandler = father;
-            Console.WriteLine(text);
+        }
+
+        public bool CreateConvexHauls(List<Point> points)
+        {
+
+            List<List<Point>> finalResultList = new List<List<Point>>();
+
+            foreach (List<Point> list in ConvexHaul.CalculateHull(points, AreaAroundYuu))
+            {
+                finalResultList.Add(list);
+            }
+            foreach (List<Point> list in ConvexHaul.CalculateHull(points, NeighourHood))
+            {
+                finalResultList.Add(list);
+            }
+            foreach (List<Point> list in ConvexHaul.CalculateHull(points, Town))
+            {
+                finalResultList.Add(list);
+            }
+            foreach (List<Point> list in ConvexHaul.CalculateHull(points, Country))
+            {
+                finalResultList.Add(list);
+            }
+
+            String JSON = "{result : [";
+
+            foreach (var list in finalResultList)
+            {
+                JSON = JSON + "[";
+                foreach (var point in list)
+                {
+                    JSON = JSON + "{" + point.x + ", " + point.y + "},";
+                }
+                JSON = JSON.TrimEnd(',');
+                JSON = JSON + "],";
+            }
+
+            JSON = JSON.TrimEnd(',');
+            JSON = JSON + "]}";
+
+            Console.WriteLine(JSON);
+
+            return true;
+        }
+
+        public bool CheckIfPointsCauseAlert(List<Point> points, Point user)
+        {
+            String JSON = "{areas : [";
+
+            int counterForAreaAroundYuu = 0;
+            int counterForNeighourHood = 0;
+            int counterForTown = 0;
+            int counterForCountry = 0;
+
+            foreach (Point point in points)
+            {
+                if (ConvexHaul.Distance(point, user) < AreaAroundYuu)
+                    counterForAreaAroundYuu++;
+                if (ConvexHaul.Distance(point, user) < NeighourHood)
+                    counterForNeighourHood++;
+                if (ConvexHaul.Distance(point, user) < Town)
+                    counterForTown++;
+                if (ConvexHaul.Distance(point, user) < Country)
+                    counterForCountry++;
+            }
+
+            if (counterForAreaAroundYuu >= AreaAroundYuuCases)
+            {
+                JSON = JSON + "{\"AreaAroundYou\" : 1},";
+            }
+            else
+            {
+                JSON = JSON + "{\"AreaAroundYou\" : 0},";
+            }
+
+            if (counterForNeighourHood >= NeighourHoodCases)
+            {
+                JSON = JSON + "{\"NeighourHood\" : 1},";
+            }
+            else
+            {
+                JSON = JSON + "{\"NeighourHood\" : 0},";
+            }
+
+            if (counterForTown >= TownCases)
+            {
+                JSON = JSON + "{\"Town\" : 1},";
+            }
+            else
+            {
+                JSON = JSON + "{\"Town\" : 0},";
+            }
+
+            if (counterForCountry >= CountryCases)
+            {
+                JSON = JSON + "{\"Country\" : 1}]}";
+            }
+            else
+            {
+                JSON = JSON + "{\"Country\" : 1}]}";
+            }
+
+            return false;
         }
 
         public override bool InvokeCommand(SubModuleFunctions command, IContext contextHandler)
@@ -31,135 +135,34 @@ namespace IP_Framework
 
             EpidemyContext subModuleContextHandler = contextHandler as EpidemyContext;
 
-            DataBaseContext context = new DataBaseContext();
-            Dictionary<string, string> dictionary = new Dictionary<string, string>(); // the params
-            EventHandlerContext commandContext = new EventHandlerContext();
+            DBModule instance = Utils.Singleton<DBModule>.Instance;
+            UserHandler userHandler = instance.GetUserHandler();
+            List<Point> points;
 
             switch (command)
             {
 
                 case SubModuleFunctions.EpidemyCheckForAreas:
 
-                    context.databaseId = DataBaseDefines.DatabaseDiseases; // the database id;
-                    context.databaseFunctionId = DataBaseDefines.GetAllDots; // function ID
-
-
-                    dictionary.Add("x", subModuleContextHandler.x.ToString());
-                    dictionary.Add("y", subModuleContextHandler.y.ToString());
-                    dictionary.Add("disease", subModuleContextHandler.specificSearch);
-                    context.ParametersDictionary = dictionary;
-
-
-                    commandContext.command = EventHandlerFunctions.DataBaseModule;
-                    commandContext.subModuleCommand = SubModuleFunctions.DataBaseQueryData;
-                    commandContext.coreCommand = CoreKnownFunctions.InvalidCommand;
-                    commandContext.contextHandler = context;
-
-
-                    bool resultcontextHool = fatherHandler.InvokeCommand(commandContext);
-                    if (!resultcontextHool)
-                    {
-                        return false;
-                    }
-
-                    resultcontextHool = fatherHandler.InvokeCommand(commandContext);
-
-                    List<Point> points = context.points;
-
-                    List<List<Point>> finalResultList = new List<List<Point>>();
-
-                    foreach (List<Point> list in ConvexHaul.CalculateHull(points, AreaAroundYuu))
-                    {
-                        finalResultList.Add(list);
-                    }
-                    foreach (List<Point> list in ConvexHaul.CalculateHull(points, NeighourHood))
-                    {
-                        finalResultList.Add(list);
-                    }
-                    foreach (List<Point> list in ConvexHaul.CalculateHull(points, Town))
-                    {
-                        finalResultList.Add(list);
-                    }
-                    foreach (List<Point> list in ConvexHaul.CalculateHull(points, Country))
-                    {
-                        finalResultList.Add(list);
-                    }
-
-                    // CALL frontend care primeste List<List<Point>> si afiseaza valorile si x-y nostru
-
-
+                    points = userHandler.GetPoints();
+                    CreateConvexHauls(points);
                     return true;
 
                 case SubModuleFunctions.EpidemyCheckForSpecificAlert:
 
-                    context.databaseId = DataBaseDefines.DatabaseDiseases; // the database id;
-                    context.databaseFunctionId = DataBaseDefines.DiseasesSpecificQueryDisease; // function ID
-
-                    dictionary.Add("x", subModuleContextHandler.x.ToString());
-                    dictionary.Add("y", subModuleContextHandler.y.ToString());
-                    dictionary.Add("disease", subModuleContextHandler.specificSearch);
-                    context.ParametersDictionary = dictionary;
-
-                    commandContext.command = EventHandlerFunctions.DataBaseModule;
-                    commandContext.subModuleCommand = SubModuleFunctions.DataBaseQueryData;
-                    commandContext.coreCommand = CoreKnownFunctions.InvalidCommand;
-                    commandContext.contextHandler = context;
-
-                    bool resultSpecific = fatherHandler.InvokeCommand(commandContext);
-
-                    if (!resultSpecific)
-                    {
-                        return false;
-                    }
-
-                    foreach (KeyValuePair<string, string> entry in context.AnswerDictionary)
-                    {
-                        // Validate each response
-
-                        subModuleContextHandler.AnswerDictionary[entry.Key] = entry.Value; // perechi boala - zona
-                    }
-
-                    if (subModuleContextHandler.AnswerDictionary.Count == 0)
-                    {
-                        return false;
-                    }
-
+                    points = userHandler.GetPointsForDisease(subModuleContextHandler.specificSearch);
+                    CreateConvexHauls(points);
                     return true;
+
                 case SubModuleFunctions.EpidemyCheckForAlert:
 
-                    context.databaseId = DataBaseDefines.DatabaseDiseases; // the database id;
-                    context.databaseFunctionId = DataBaseDefines.DiseasesFullQuery; // function ID
-
-                    dictionary.Add("x", subModuleContextHandler.x.ToString());
-                    dictionary.Add("y", subModuleContextHandler.y.ToString());
-                    context.ParametersDictionary = dictionary;
-
-                    commandContext.command = EventHandlerFunctions.DataBaseModule;
-                    commandContext.subModuleCommand = SubModuleFunctions.DataBaseQueryData;
-                    commandContext.coreCommand = CoreKnownFunctions.InvalidCommand;
-                    commandContext.contextHandler = context;
-
-                    bool result = fatherHandler.InvokeCommand(commandContext);
-
-                    if (!result)
-                    {
-                        return false;
-                    }
-
-                    foreach (KeyValuePair<string, string> entry in context.AnswerDictionary)
-                    {
-                        // Validate each response
-
-                        subModuleContextHandler.AnswerDictionary[entry.Key] = entry.Value; // perechi boala - zona
-                    }
-
-                    if (subModuleContextHandler.AnswerDictionary.Count == 0)
-                    {
-                        return false;
-                    }
-
+                    points = userHandler.GetPointsForDisease(subModuleContextHandler.specificSearch);
+                    Point user = new Point();
+                    CheckIfPointsCauseAlert(points, user);
                     return true;
+
                 default:
+
                     return false;
             }
         }
